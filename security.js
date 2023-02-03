bdc5c7e700bdd249a96b5a40fc3637f49e270811
Michael Glenn <pimpneasily@yahoo.com>
2023-02-02 20:58:21 -0600

    Create security.js

---
+++ security.js
@@ -0,0 +1,48 @@
+Next: GNU guile - Scheme, Previous: C#, Up: Individual Programming Languages   [Contents][Index]
+
+15.5.5 JavaScript
+RPMs
+js
+
+Ubuntu packages
+gjs
+
+File extension
+js
+
+String syntax
+"abc"
+'abc'
+`abc`
+gettext shorthand
+_("abc")
+
+gettext/ngettext functions
+gettext, dgettext, dcgettext, ngettext, dngettext
+
+textdomain
+textdomain function
+
+bindtextdomain
+bindtextdomain function
+
+setlocale
+automatic
+
+Prerequisite
+—
+
+Use or emulate GNU gettext
+use, or emulate
+
+Extractor
+xgettext
+
+Formatting with positions
+—
+
+Portability
+On platforms without gettext, the functions are not available.
+
+po-mode marking
+—
